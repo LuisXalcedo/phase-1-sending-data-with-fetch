00476c90e6890def31638784a08f79988dad233d
Luis Salcedo <luis.e.ss@gmail.com>
2023-04-25 19:40:40 -0600

    Complete sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData(name, email) {
+  const formData = {
+    name: name,
+    email: email,
+  };
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  const send = fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      let p = document.createElement("p");
+      p.textContent = object.id;
+      document.querySelector("body").appendChild(p);
+    })
+    .catch(function (error) {
+      let p = document.createElement("p");
+      p.textContent = error.message;
+      document.querySelector("body").appendChild(p);
+    });
+
+  return send;
+}
